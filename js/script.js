d4c132336659a8bb26e09656371a3cf51187c6fe
osdal <osdal67@gmail.com>
2018-08-27 22:35:57 +0300

    посим кhyew

--- js/script.js
+++ js/script.js
@@ -217,6 +217,7 @@ let contact_form = document.querySelector('.contact-form form');
 		prev = document.querySelector('.prev'),
 		next = document.querySelector('.next'),
 		dotsWrap = document.querySelector('.slider-dots');
+		dots = document.getElementsByClassName('dot');
 
 		showSlides(slideIndex);
 
@@ -228,6 +229,10 @@ let contact_form = document.querySelector('.contact-form form');
 				slideIndex = slides.length;
 			};
 
+			for (let i = 0; i < slides.length; i ++) {
+				slides[i].style.display = 'none';		
+			}
+
 			for (let i = 0; i < dots.length; i ++) {
 				dots[i].classList.remove('dot-active');
 			};
@@ -237,8 +242,69 @@ let contact_form = document.querySelector('.contact-form form');
 		}
 
 		function plusSlides (n) {
-			showSlides(slideIndex) += n;
+			showSlides(slideIndex += n);
 		}
+
+		function currentSlide (n) {
+			showSlides(slideIndex = n);
+		}
+
+		prev.addEventListener('click', function () {
+			plusSlides (-1);
+		});
+
+		next.addEventListener('click', function () {
+			plusSlides (1);
+		});
+
+		dotsWrap.addEventListener('click', function (event) {
+			for (let i = 0; i < dots.length + 1; i++) {
+				if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+					currentSlide(i);
+				}
+			}
+		});
+
+		// Calc
+
+		let persons = document.getElementsByClassName('counter-block-input')[0],
+			restDays = document.getElementsByClassName('counter-block-input')[1],
+			place = document.getElementById('select'),
+			totalValue = document.getElementById('total'),
+			personsSum = 0,
+			daysSum = 0,
+			total = 0;
+
+			totalValue.innerHTML = 0;
+
+			persons.addEventListener('change', function () {
+				personsSum = +this.value;
+				total = (daysSum + personsSum) * 4000;
+				if (restDays.value == '') {
+					totalValue.innerHTML = 0;
+				} else {
+					totalValue.innerHTML = total;
+				}
+			});
+
+			restDays.addEventListener('change', function() {
+				daysSum = +this.value;
+				total = (daysSum + personsSum) * 4000;
+				if (persons.value == '') {
+					totalValue.innerHTML = 0;
+				} else {
+					totalValue.innerHTML = total;
+				}
+			});
+
+			place.addEventListener('change', function() {
+				if (restDays.value == '' || persons.value == '') {
+					totalValue.innerHTML = 0;
+				} else {
+					let a = total;
+					totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+				}
+			});
 
 
  
